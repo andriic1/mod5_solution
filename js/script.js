8c6f8382799cc32e95c4a7f4bc48e3c971fb1803
andriic1 <55826535+andriic1@users.noreply.github.com>
2026-06-07 22:34:03 +0300

    Refactor AJAX calls and clean up comments
    Refactor AJAX requests and remove TODO comments for clarity.

--- js/script.js
+++ js/script.js
@@ -61,19 +61,10 @@ $(function () {
 
   // On page load (before images or CSS)
   document.addEventListener("DOMContentLoaded", function (event) {
-    // TODO: STEP 0: Look over the code from
-    // *** start ***
-    // to
-    // *** finish ***
-
     // *** start ***
     // On first load, show home view
     showLoading("#main-content");
-    $ajaxUtils.sendGetRequest(
-      allCategoriesUrl,
-      // ***** <---- TODO: STEP 1: Substitute [...] ******
-      true,
-    ); // Explicitly setting the flag to get JSON from server processed into an object literal
+    $ajaxUtils.sendGetRequest(allCategoriesUrl, buildAndShowHomeHTML, true); // Explicitly setting the flag to get JSON from server processed into an object literal
   });
   // *** finish **
 
@@ -84,32 +75,47 @@ $(function () {
     $ajaxUtils.sendGetRequest(
       homeHtmlUrl,
       function (homeHtml) {
-        // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
-        // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
-        // variable's name implies it expects.
+        function buildAndShowMenuItemsHTML(categoryMenuItems) {
+          // Load title snippet of menu items page
+          $ajaxUtils.sendGetRequest(
+            menuItemsTitleHtml,
+            function (menuItemsTitleHtml) {
+              // Retrieve single menu item snippet
+              $ajaxUtils.sendGetRequest(
+                menuItemHtml,
+                function (menuItemHtml) {
+                  var menuItemsViewHtml = buildMenuItemsViewHtml(
+                    categoryMenuItems,
+                    menuItemsTitleHtml,
+                    menuItemHtml,
+                  );
+                  insertHtml("#main-content", menuItemsViewHtml);
+                },
+                false,
+              );
+            },
+            false,
+          );
+        }
+
         var chosenCategoryShortName =
           chooseRandomCategory(categories).short_name;
 
-        // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
-        // chosen category from STEP 2. Use existing insertProperty function for that purpose.
-        // Look through this code for an example of how to do use the insertProperty function.
-        // WARNING! You are inserting something that will have to result in a valid Javascript
-        // syntax because the substitution of {{randomCategoryShortName}} becomes an argument
-        // being passed into the $dc.loadMenuItems function. Think about what that argument needs
-        // to look like. For example, a valid call would look something like this:
-        // $dc.loadMenuItems('L')
-        // Hint: you need to surround the chosen category short name with something before inserting
-        // it into the home html snippet.
-        //
-        // var homeHtmlToInsertIntoMainPage = ....
+        $dc.loadMenuItems("L");
 
-        // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
-        // Use the existing insertHtml function for that purpose. Look through this code for an example
-        // of how to do that.
-        // ....
+        var homeHtmlToInsertIntoMainPage = insertProperty(
+          homeHtml,
+          "randomCategoryShortName",
+          "'" + chosenCategoryShortName + "'",
+        );
+
+        insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
+
+        var finalHtml = categoriesTitleHtml;
+        finalHtml += "<section class='row'>";
       },
-      false, // False here because we are getting just regular HTML from the server, so no need to process JSON.
-    );
+      false,
+    ); // False here because we are getting just regular HTML from the server, so no need to process JSON.
   }
 
   // Given array of category objects, returns a random category object.
